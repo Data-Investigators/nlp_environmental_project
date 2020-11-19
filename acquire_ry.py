ada22380d0d42be47211bf2c4f55278e787398c8
RyvynYoung <ryvyn.young@gmail.com>
2020-11-19 17:26:14 -0600

    new function call is get_github2

--- acquire_ry.py
+++ acquire_ry.py
@@ -80,7 +80,7 @@ def github_urls():
     the github search page and returns a list of urls.
     '''
     # get the first 15 pages to allow for those that don't have readme or language
-    pages = range(51, 100)
+    pages = range(1, 100)
     urls = []
     
     for p in pages:
@@ -99,14 +99,129 @@ def github_urls():
         page_urls = list(page_urls)
         # append the list from the page to the full list to return
         urls.append(page_urls)
-        time.sleep(3)
+        time.sleep(10)
         
     # flatten the urls list
     urls = [y for x in urls for y in x]
     return urls
 # this produces a flattened list of lists = 270 urls
 
-def get_github(cached=False):
+###### from Corey
+def get_urls_list():
+    '''
+    This function collects Github repos when given a URL search page
+    '''
+    urls = ['https://github.com/freeCodeCamp/freeCodeCamp',
+        'https://github.com/Famous/famous',
+        'https://github.com/vuejs/vue',
+        'https://github.com/kevana/ui-for-docker',
+        'https://github.com/facebook/react',
+        'https://github.com/ipython/ipython',
+        'https://github.com/microsoft/TypeScript-Handbook',
+        'https://github.com/airbnb/knowledge-repo',
+        'https://github.com/rkern/line_profiler',
+        'https://github.com/babel/babel-preset-env',
+        'https://github.com/s3tools/s3cmd',
+        'https://github.com/zzzeek/sqlalchemy',
+        'https://github.com/thunil/TecoGAN',
+        'https://github.com/l1ving/youtube-dl',
+        'https://github.com/mdo/github-buttons',
+        'https://github.com/StephenGrider/ReactNativeReduxCasts',
+        'https://github.com/apollographql/apollo',
+        'https://github.com/wandb/client',
+        'https://github.com/RasaHQ/rasa_core',
+        'https://github.com/angular-ui/angular-ui-OLDREPO',
+        'https://github.com/urwid/urwid',
+        'https://github.com/timqian/star-history',
+        'https://github.com/PatrickJS/NG6-starter',
+        'https://github.com/knrt10/kubernetes-basicLearning',
+        'https://github.com/nature-of-code/noc-book',
+        'https://github.com/erikbern/git-of-theseus',
+        'https://github.com/dortania/OpenCore-Install-Guide',
+        'https://github.com/Kapeli/Dash-User-Contributions',
+        'https://github.com/github/docs',
+        'https://github.com/StephenGrider/redux-code',
+        'https://github.com/fossasia/meilix',
+        'https://github.com/StephenGrider/EthereumCasts',
+        'https://github.com/newren/git-filter-repo',
+        'https://github.com/mateodelnorte/meta',
+        'https://github.com/arsaboo/homeassistant-config',
+        'https://github.com/IronLanguages/main',
+        'https://github.com/StephenGrider/FullstackReactCode',
+        'https://github.com/openworm/OpenWorm',
+        'https://github.com/apache/nano',
+        'https://github.com/jupyterhub/repo2docker',
+        'https://github.com/abidrahmank/OpenCV2-Python-Tutorials',
+        'https://github.com/Ceruleanacg/Personae',
+        'https://github.com/mtdvio/ru-tech-chats',
+        'https://github.com/xinntao/EDVR',
+        'https://github.com/RubensZimbres/Repo-2017',
+        'https://github.com/ipfs-inactive/js-ipfs-http-client',
+        'https://github.com/browserpass/browserpass-legacy',
+        'https://github.com/boston-dynamics/spot-sdk',
+        'https://github.com/sourcerer-io/hall-of-fame',
+        'https://github.com/blackorbird/APT_REPORT',
+        'https://github.com/creationix/howtonode.org',
+        'https://github.com/jennschiffer/make8bitart',
+        'https://github.com/wdas/reposado',
+        'https://github.com/guyzmo/git-repo',
+        'https://github.com/Netflix/repokid',
+        'https://github.com/nosarthur/gita',
+        'https://github.com/harshjv/github-repo-size',
+        'https://github.com/babel/babel-standalone',
+        'https://github.com/kevin28520/My-TensorFlow-tutorials',
+        'https://github.com/diyhue/diyHue',
+        'https://github.com/StephenGrider/rn-casts',
+        'https://github.com/headsetapp/headset-electron',
+        'https://github.com/StijnMiroslav/top-starred-devs-and-repos-to-follow',
+        'https://github.com/techgaun/active-forks',
+        'https://github.com/donnemartin/viz',
+        'https://github.com/tailwindlabs/tailwindui-vue',
+        'https://github.com/GitGuardian/gg-shield',
+        'https://github.com/dtschust/redux-bug-reporter',
+        'https://github.com/burke-software/django-report-builder',
+        'https://github.com/antsmartian/lets-build-express',
+        'https://github.com/MicrosoftDocs/visualstudio-docs',
+        'https://github.com/earwig/git-repo-updater',
+        'https://github.com/OpenSourceTogether/Hacktoberfest-2020',
+        'https://github.com/lightaime/deep_gcns_torch',
+        'https://github.com/A3M4/YouTube-Report',
+        'https://github.com/heroku/heroku-repo',
+        'https://github.com/lambdaji/tf_repos',
+        'https://github.com/StephenGrider/AdvancedReactNative',
+        'https://github.com/lightaime/deep_gcns',
+        'https://github.com/StephenGrider/DockerCasts',
+        'https://github.com/mappum/gitbanner',
+        'https://github.com/declare-lab/conv-emotion',
+        'https://github.com/MarkWuNLP/MultiTurnResponseSelection',
+        'https://github.com/chriswhong/nyctaxi',
+        'https://github.com/18F/analytics-reporter',
+        'https://github.com/npmhub/npmhub',
+        'https://github.com/kesiev/akihabara',
+        'https://github.com/ionic-team/ionic-site',
+        'https://github.com/ros/rosdistro',
+        'https://github.com/GoogleChromeLabs/tooling.report',
+        'https://github.com/rpl/flow-coverage-report',
+        'https://github.com/storybook-eol/react-native-storybook',
+        'https://github.com/StephenGrider/MongoCasts',
+        'https://github.com/kundajelab/deeplift',
+        'https://github.com/microsoft/HealthClinic.biz',
+        'https://github.com/Redocly/create-openapi-repo',
+        'https://github.com/philbooth/complexity-report',
+        'https://github.com/bensmithett/webpack-css-example',
+        'https://github.com/PhantomInsights/mexican-government-report',
+        'https://github.com/googlefonts/Inconsolata',
+        'https://github.com/laincloud/lain',
+        'https://github.com/auth0/repo-supervisor',
+        'https://github.com/weightagnostic/weightagnostic.github.io',
+        'https://github.com/jdorn/php-reports',
+        'https://github.com/joeldenning/coexisting-vue-microfrontends']
+
+    return urls
+
+
+
+def get_github2(cached=False):
     '''
     This function with default cached == False does a fresh scrape of github pages returned from
     search of 'environmental' and writes the returned df to a json file.
@@ -114,7 +229,7 @@ def get_github(cached=False):
     '''
     # option to read in a json file instead of scrape for df
     if cached == True:
-        df = pd.read_json('readmes.json')
+        df = pd.read_json('readmes2.json')
         
     # cached == False completes a fresh scrape for df    
     else:
@@ -158,10 +273,10 @@ def get_github(cached=False):
         df = pd.DataFrame(readmes)
 
         # Write df to a json file for faster access
-        df.to_json('readmes.json')
+        df.to_json('readmes2.json')
 
     return df
-    # this returns df with 208 observations
+
 
 
 def get_github_86results(cached=False):
